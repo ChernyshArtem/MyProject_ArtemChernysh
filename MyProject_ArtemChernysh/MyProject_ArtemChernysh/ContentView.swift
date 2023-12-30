31b67aaef5142704dc01f52661aee0e1d5938634
unknown <korolenkosergei222@gmail.com>
2023-12-30 22:20:38 +0300

    Sergei

--- MyProject_ArtemChernysh/MyProject_ArtemChernysh/ContentView.swift
+++ MyProject_ArtemChernysh/MyProject_ArtemChernysh/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!+ denis+ artem")
+            Text("Hello, world!+ sergei+ denis+ artem")
         }
         .padding()
     }
