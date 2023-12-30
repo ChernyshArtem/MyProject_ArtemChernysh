b9516d1f84d7a2594fbcaecede145791b98f890f
unknown <korolenkosergei222@gmail.com>
2023-12-31 00:45:15 +0300

    SergeiBranch

---
+++ MyProject_ArtemChernysh/MyProject_ArtemChernysh/SergeiViev.swift
@@ -0,0 +1,18 @@
+//
+//  ArtemChernyshView.swift
+//  MyProject_ArtemChernysh
+//
+//  Created by Артём Черныш on 30.12.23.
+//
+
+import SwiftUI
+
+struct ArtemChernyshView: View {
+    var body: some View {
+        Text("Hello Sergei Artem Denis!")
+    }
+}
+
+#Preview {
+    ArtemChernyshView()
+}
