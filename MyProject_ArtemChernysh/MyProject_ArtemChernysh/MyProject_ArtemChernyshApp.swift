2374c6527120475e4428f37a3c4ede3fb9a2c031
Артём Черныш <artemchernysh@192.168.1.73>
2023-12-30 18:28:27 +0300

    Add Hello world project

---
+++ MyProject_ArtemChernysh/MyProject_ArtemChernysh/MyProject_ArtemChernyshApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProject_ArtemChernyshApp.swift
+//  MyProject_ArtemChernysh
+//
+//  Created by Артём Черныш on 30.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MyProject_ArtemChernyshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
