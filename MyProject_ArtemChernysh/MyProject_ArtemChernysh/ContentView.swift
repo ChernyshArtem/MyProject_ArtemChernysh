78e85f39661c0ea2f6ca9f591e8a03de0ad282fa
Артём Черныш <artemchernysh@192.168.1.73>
2023-12-30 20:51:02 +0300

    Add second feature

--- MyProject_ArtemChernysh/MyProject_ArtemChernysh/ContentView.swift
+++ MyProject_ArtemChernysh/MyProject_ArtemChernysh/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView2: View {
     var body: some View {
         VStack {
             Text("Hello, world! From Belarus")
@@ -17,5 +17,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView2()
 }
